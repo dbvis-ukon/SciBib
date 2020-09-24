53adeb5c052404120ef283343ced9a8b3db375bc
Benedikt Bäumle <benedikt.baeumle@uni-konstanz.de>
2020-09-24 16:56:45 +0200

    add license header to files

--- app/backend/modules/api.py
+++ app/backend/modules/api.py
@@ -8,6 +8,35 @@ The rest of the relevant info can be found in: https://github.com/dbvis-ukon/Sci
 Further comments: tojson function MUST HAVE the same output as currently
 """
 
+#  Copyright (C) 2020 University of Konstanz -  Data Analysis and Visualization Group
+#  This file is part of SciBib <https://github.com/dbvis-ukon/SciBib>.
+#
+#  SciBib is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  SciBib is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with SciBib.  If not, see <http://www.gnu.org/licenses/>.
+#
+#  SciBib is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  SciBib is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with SciBib.  If not, see <http://www.gnu.org/licenses/>.
+
 from backend.db_controller.db import *
 from backend.db_controller.query.authors import getAuthors, getAuthorByNames
 from backend.db_controller.query.categories import getCategories
--- app/frontend/static/js/index.js
+++ app/frontend/static/js/index.js
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2020 University of Konstanz -  Data Analysis and Visualization Group
+ * This file is part of SciBib <https://github.com/dbvis-ukon/SciBib>.
+ *
+ * SciBib is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SciBib is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SciBib.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/main.css';
 import $ from 'jquery';
