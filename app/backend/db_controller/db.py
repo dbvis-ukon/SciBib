d643037a02d34fb2f9060524718b16d021bf4e1c
Benedikt Bäumle <benedikt.baeumle@uni-konstanz.de>
2020-09-25 13:53:55 +0200

    fix broken license headers

--- app/backend/db_controller/db.py
+++ app/backend/db_controller/db.py
@@ -1,8 +1,3 @@
-"""
-
-DB_Controller for the scibib database
-
-"""
 #  Copyright (C) 2020 University of Konstanz -  Data Analysis and Visualization Group
 #  This file is part of SciBib <https://github.com/dbvis-ukon/SciBib>.
 #
@@ -18,19 +13,12 @@ DB_Controller for the scibib database
 #
 #  You should have received a copy of the GNU General Public License
 #  along with SciBib.  If not, see <http://www.gnu.org/licenses/>.
-#
-#  SciBib is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
-#
-#  SciBib is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with SciBib.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+
+DB_Controller for the scibib database
+
+"""
 
 from flask_sqlalchemy import SQLAlchemy as _BaseSQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
